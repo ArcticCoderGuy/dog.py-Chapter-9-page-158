6d610e3c4e5f6bb39ae1362d33b2be74ebc402a9
Markus Kaprio <markus.kaprio@edunord.se>
2025-11-29 12:10:27 +0200

    1st.  block

---
+++ Dog.py
@@ -0,0 +1,12 @@
+
+class Dog:
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name: str, breed: str, age: int) -> None:
+        """Initialize attributes to describe a dog."""
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+#joo on se selaista# 
+
