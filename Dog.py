041293ab41cf9a5da87b82f8135cdebea2fadbd8
Markus Kaprio <markus.kaprio@edunord.se>
2025-11-29 13:01:34 +0200

    Made the class with more instructions so it can make more complex instances

--- Dog.py
+++ Dog.py
@@ -8,5 +8,22 @@ class Dog:
         self.breed = breed
         self.age = age
 
-#joo on se selaista# 
+    def sit(self) -> None:
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")    
+
+    def roll_over(self) -> None:
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")    
+
+
+
+     def introduce(self) -> None:
+        """Print an introduction of the dog.   """
+        print(f"hi, I´m {self.name}, a {self.age}-year-old {self.breed}.")
+
+        Mr.King = Dog('Mr.King', 'Golden Retriever', 6)
+        Mr.King.introduce()
+
+
 
